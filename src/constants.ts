feea1477a966c4cd6b7d1a0d1e377d096dec671e
codebysid <codebysidd@gmail.com>
2024-01-16 19:40:04 +0530

    constant data changed

--- src/constants.ts
+++ src/constants.ts
@@ -4,69 +4,59 @@ export interface SearchOptionsDataInterface {
 }
 const searchOptions: SearchOptionsDataInterface[] = [
   {
-    username: "IronMan",
+    username: "John Doe",
     userPicture:
-      "https://i.pinimg.com/564x/c8/17/60/c81760d77b8c93a06d51ceb8cdda851f.jpg",
+      "https://i.pinimg.com/564x/a6/34/34/a63434e83afd75c592b81d9775e44277.jpg",
   },
   {
-    username: "Batman",
+    username: "Rakesh Sharma",
     userPicture:
-      "https://i.pinimg.com/564x/fc/49/fe/fc49fef80f27f04a24346ece533199e8.jpg",
+      "https://i.pinimg.com/564x/96/5b/f0/965bf0ffaa83d1a76d7bcaf9c30c9436.jpg",
   },
   {
-    username: "Hulk",
+    username: "Preety Sharma",
     userPicture:
-      "https://i.pinimg.com/564x/14/47/77/1447772e26709e7e7001ad3ecd31053a.jpg",
+      "https://i.pinimg.com/564x/a6/34/34/a63434e83afd75c592b81d9775e44277.jpg",
   },
   {
-    username: "Thor",
+    username: "Siddhant Jain",
     userPicture:
-      "https://i.pinimg.com/564x/f0/d9/bf/f0d9bf7e5591243cb747b9a5bfe1a8f8.jpg",
+      "https://i.pinimg.com/564x/96/5b/f0/965bf0ffaa83d1a76d7bcaf9c30c9436.jpg",
   },
   {
-    username: "Joker",
+    username: "Yash Tomar",
     userPicture:
-      "https://i.pinimg.com/564x/e6/01/23/e601237f787d4c123bd1b2984f4495b3.jpg",
+      "https://i.pinimg.com/564x/97/4c/9c/974c9c2446eb62327642dbea0f5f1502.jpg",
   },
   {
-    username: "Black Widow",
+    username: "Rishabh Jain",
     userPicture:
-      "https://i.pinimg.com/564x/3b/e6/f3/3be6f370131afb72d33d2e9950f7816d.jpg",
+      "https://i.pinimg.com/564x/97/4c/9c/974c9c2446eb62327642dbea0f5f1502.jpg",
   },
   {
-    username: "Thanos",
+    username: "Rahul Shinde",
     userPicture:
-      "https://i.pinimg.com/564x/cb/6e/63/cb6e638c58d91ef6a84a0292349256e1.jpg",
+      "https://i.pinimg.com/564x/d3/7b/02/d37b020e87945ad7f245e48df752ed03.jpg",
   },
   {
-    username: "Wanda",
+    username: "Ritu Chaudhary",
     userPicture:
-      "https://i.pinimg.com/564x/f0/2d/64/f02d64ecf293e14bf387aa1554dde9fe.jpg",
+      "https://i.pinimg.com/564x/97/4c/9c/974c9c2446eb62327642dbea0f5f1502.jpg",
   },
   {
-    username: "Thor",
+    username: "Vyom Singh",
     userPicture:
-      "https://i.pinimg.com/564x/f0/d9/bf/f0d9bf7e5591243cb747b9a5bfe1a8f8.jpg",
+      "https://i.pinimg.com/564x/d3/7b/02/d37b020e87945ad7f245e48df752ed03.jpg",
   },
   {
-    username: "Joker",
+    username: "Prakhar Tomar",
     userPicture:
-      "https://i.pinimg.com/564x/e6/01/23/e601237f787d4c123bd1b2984f4495b3.jpg",
+      "https://i.pinimg.com/564x/a6/34/34/a63434e83afd75c592b81d9775e44277.jpg",
   },
   {
-    username: "Black Widow",
+    username: "Aditya Kapoor",
     userPicture:
-      "https://i.pinimg.com/564x/3b/e6/f3/3be6f370131afb72d33d2e9950f7816d.jpg",
-  },
-  {
-    username: "Thanos",
-    userPicture:
-      "https://i.pinimg.com/564x/cb/6e/63/cb6e638c58d91ef6a84a0292349256e1.jpg",
-  },
-  {
-    username: "Wanda",
-    userPicture:
-      "https://i.pinimg.com/564x/f0/2d/64/f02d64ecf293e14bf387aa1554dde9fe.jpg",
+      "https://i.pinimg.com/564x/d3/7b/02/d37b020e87945ad7f245e48df752ed03.jpg",
   },
 ];
 
